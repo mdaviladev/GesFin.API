968dd8e8b1a92626e21ffbacfb0990d92e308484
Marcelo Duarte de Ávila <marceloduarte.avila@hotmail.com>
2024-12-17 18:52:35 -0300

    Configuração de autorização de acesso no app.razor

--- GesFin.Web/Program.cs
+++ GesFin.Web/Program.cs
@@ -8,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<CookieHandler>();
+
 builder.Services.AddMudServices();
 
 builder.Services
